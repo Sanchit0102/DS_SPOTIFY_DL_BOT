fb22a45bf664d291b41fb91f26f4e2236dfc6c91
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-08-05 09:41:53 +0530

    Update fsub.py

--- fsub.py
+++ fsub.py
@@ -1,7 +1,7 @@
 from pyrogram.errors import UserNotParticipant, PeerIdInvalid, UserIsBlocked, ChatWriteForbidden
 from pyrogram import enums, filters, StopPropagation
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from mbot import Mbot, LOG_GROUP as BUG,F_SUB,F_SUB_CHANNEL_ID, F_SUB_CHANNEL_INVITE_LINK
+from mbot import Mbot, LOG_GROUP as BUG, F_SUB, F_SUB_CHANNEL_ID, F_SUB_CHANNEL_INVITE_LINK
 from requests import post 
 import traceback 
 def paste(text):
@@ -12,18 +12,12 @@ def paste(text):
 
 async def Fsub(message, Mbot, user_id):
       try:
-          if user_id  == 5268375124:
+          if user_id  == 6302879150:
              return 
           try:
               get_member = await Mbot.get_chat_member(chat_id=F_SUB_CHANNEL_ID,user_id=user_id)
           except UserNotParticipant:
-              await message.reply(
-    "Sᴏʀʀʏ Sɪʀ/ Mᴀᴅᴀᴍ 🥲\n\n"
-    "    Iɴ ᴏʀᴅᴇʀ ᴛᴏ ᴜsᴇ ᴍᴇ, ᴘʟᴇᴀsᴇ ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ\n\n"
-    "Nᴏᴛᴇ:\n"
-    "    Iғ ʏᴏᴜ ᴇɴᴄᴏᴜɴᴛᴇʀ ᴀɴʏ ɪssᴜᴇs, ʏᴏᴜ ᴄᴀɴ ʙᴜʏ ᴘʀᴇᴍɪᴜᴍ ᴛᴏ sᴋɪᴘ ᴊᴏɪɴɪɴɢ ᴛʜᴇ ᴄʜᴀɴɴᴇʟ.\n\n"
-    "Pʟᴇᴀsᴇ ᴊᴏɪɴ ᴛʜᴇ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴜsᴇ ᴍᴇ!\n"
-    "Jᴏɪɴ ᴛʜᴇ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴘʀᴇss ᴛʜᴇ ʀᴇғʀᴇsʜ ʙᴜᴛᴛᴏɴ.",
+              await message.reply("<b>Please Join My Updates Channel to use this Bot!\n\nDue to Overload, Only Channel Subscribers can use this Bot!</b>",
     reply_markup=InlineKeyboardMarkup([
          [InlineKeyboardButton("Join Channel 📣", url=F_SUB_CHANNEL_INVITE_LINK)],
         [InlineKeyboardButton("Refresh 🔄", callback_data="refresh")]
@@ -39,13 +33,7 @@ async def Fsub(message, Mbot, user_id):
               except UserIsBlocked:
                   pass
               except UserNotParticipant:
-                  await message.reply(
-    "Sᴏʀʀʏ Sɪʀ/ Mᴀᴅᴀᴍ 🥲\n\n"
-    "    Iɴ ᴏʀᴅᴇʀ ᴛᴏ ᴜsᴇ ᴍᴇ, ᴘʟᴇᴀsᴇ ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ\n\n"
-    "Nᴏᴛᴇ:\n"
-    "    Iғ ʏᴏᴜ ᴇɴᴄᴏᴜɴᴛᴇʀ ᴀɴʏ ɪssᴜᴇs, ʏᴏᴜ ᴄᴀɴ ʙᴜʏ ᴘʀᴇᴍɪᴜᴍ ᴛᴏ sᴋɪᴘ ᴊᴏɪɴɪɴɢ ᴛʜᴇ ᴄʜᴀɴɴᴇʟ.\n\n"
-    "Pʟᴇᴀsᴇ ᴊᴏɪɴ ᴛʜᴇ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴜsᴇ ᴍᴇ!\n"
-    "Jᴏɪɴ ᴛʜᴇ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴘʀᴇss ᴛʜᴇ ʀᴇғʀᴇsʜ ʙᴜᴛᴛᴏɴ.",
+                  await message.reply("<b>Please Join My Updates Channel to use this Bot!\n\nDue to Overload, Only Channel Subscribers can use this Bot!</b>.",
     reply_markup=InlineKeyboardMarkup([
         [InlineKeyboardButton("Join Channel 📣", url=F_SUB_CHANNEL_INVITE_LINK)],
         [InlineKeyboardButton("Refresh 🔄", callback_data="refresh")]
